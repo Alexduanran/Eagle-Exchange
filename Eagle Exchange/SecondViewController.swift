391a7e0bc6947149fd951c041ccc0d06c40602fd
Alex Du <alexduanran@alexs-macbook-pro.local>
2018-11-15 16:16:29 -0500

    Initial Commit

---
+++ Eagle Exchange/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Eagle Exchange
+//
+//  Created by Alex Du on 11/15/18.
+//  Copyright © 2018 Alex Du. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
