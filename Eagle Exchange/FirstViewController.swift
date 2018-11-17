e77437612a1d1175262b09e85d55030d8bc86fa6
Alex Du <alexduanran@alexs-mbp.bc.edu>
2018-11-17 16:34:19 -0500

    Version with two collection views

--- Eagle Exchange/FirstViewController.swift
+++ Eagle Exchange/FirstViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var featuredCollectionView: UICollectionView!
+    @IBOutlet weak var catagoryCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        featuredCollectionView.delegate = self
+        featuredCollectionView.dataSource = self
+        
+        catagoryCollectionView.delegate = self
+        catagoryCollectionView.dataSource = self
+        
     }
 
 
 }
 
+extension FirstViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.featuredCollectionView {
+            return 3
+        } else {
+            return 3
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == self.featuredCollectionView {
+            let featuredCell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCell", for: indexPath) as UICollectionViewCell
+            return featuredCell
+        } else {
+            let catagoryCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatagoryCell", for: indexPath) as UICollectionViewCell
+            return catagoryCell
+        }
+        
+    }
+}
